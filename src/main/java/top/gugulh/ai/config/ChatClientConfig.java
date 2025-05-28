1408a9bb6821cec0f29b5847a1f5542ea481bd08
GuguLH <persistent626@outlook.com>
2025-05-28 08:47:46 +0800

    添加会话记忆

--- src/main/java/top/gugulh/ai/config/ChatClientConfig.java
+++ src/main/java/top/gugulh/ai/config/ChatClientConfig.java
@@ -1,7 +1,10 @@
 package top.gugulh.ai.config;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.MessageWindowChatMemory;
 import org.springframework.ai.deepseek.DeepSeekChatModel;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,10 +13,18 @@ import org.springframework.context.annotation.Configuration;
 public class ChatClientConfig {
 
     /**
+     * 配置ChatMemory
+     */
+    @Bean
+    public ChatMemory chatMemory() {
+        return MessageWindowChatMemory.builder().build();
+    }
+
+    /**
      * 配置客户端
      */
     @Bean
-    public ChatClient chatClient(DeepSeekChatModel model) {
+    public ChatClient chatClient(DeepSeekChatModel model, ChatMemory chatMemory) {
         String SYSTEM_PROMPT = """
                     你是一个专业的数码产品导购人员,你擅长于手机,电脑,相机等数码产品的介绍与推荐.你的名字是"小智"
                     你总是以温柔,幽默的语气回答用户提出的问题.
@@ -21,7 +32,10 @@ public class ChatClientConfig {
 
         return ChatClient.builder(model)
                 .defaultSystem(SYSTEM_PROMPT)
-                .defaultAdvisors(new SimpleLoggerAdvisor()) // 环绕增强,添加日志打印
+                .defaultAdvisors(
+                        new SimpleLoggerAdvisor(), // 环绕增强,添加日志打印
+                        MessageChatMemoryAdvisor.builder(chatMemory).build() // 会话记忆功能
+                )
                 .build();
     }
 }
--- src/main/java/top/gugulh/ai/controller/ChatController.java
+++ src/main/java/top/gugulh/ai/controller/ChatController.java
@@ -1,6 +1,8 @@
 package top.gugulh.ai.controller;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
@@ -27,9 +29,13 @@ public class ChatController {
      * 使用WebFlux完成流式响应,前端类似打字机效果
      */
     @PostMapping(value = "/chat-stream", produces = "text/html;charset=utf-8")
-    public Flux<String> chatStream(@RequestParam(name = "prompt", required = true) String prompt) {
+    public Flux<String> chatStream(
+            @RequestParam(name = "prompt", required = true) String prompt,
+            @RequestParam(name = "chatId", required = true) String chatId
+    ) {
         return chatClient.prompt()
                 .user(prompt)
+                .advisors(advisorSpec -> advisorSpec.param(ChatMemory.CONVERSATION_ID, chatId)) // 添加会话ID到Advisor上下文中
                 .stream() // 流式响应
                 .content();
     }
